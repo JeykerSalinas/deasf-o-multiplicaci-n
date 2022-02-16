9f7c78103282e40468b0860f3a3595f1d8968e5d
JeykerSalinas <66428501+JeykerSalinas@users.noreply.github.com>
2022-02-15 21:51:18 -0300

    Fixing factorial function

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,20 +1,32 @@
-const myNum = 5;
-
+const myNum = Number(prompt("Por favor ingrese número entre el 1 y el 20"))
+// Validación de datos
+if (myNum > 20 || myNum < 1 || isNaN(myNum) ) {
+  alert("Valor ingresado no válido");
+}
+// Función que imprime la multiplicación
 const multiply = (q) => {
   for (let i = 0; i <= q; i++) {
     console.log(`${i} x ${q} = ${i * q}`);
   }
 };
-
-const factorial = (num) => {
-  for (let i = 0; i <= num; i++) {
-    if (i === 0 || i === 1) {
-      console.log(1);
-    } else {
-      console.log(i * (i - 1));
+// Función que calcula el factorial de cualquier número
+const factorial = (n) => {
+  let result = 1;
+  if (n == 1) {
+    return result;
+  } else {
+    for (var i = n; i >= 1; i--) {
+      result = result * i;
     }
+    return result;
   }
 };
-multiply(myNum)
-factorial(myNum);
+// Función que imprime el factorial de cada número hasta llegar al valor ingresado
+const iteration = (num) => {
+  for (let i = 1; i <= num; i++) {
+    console.log(`El factorial de ${i} es : ${factorial(i)}`);
+  }
+};
 
+multiply(myNum);
+iteration(myNum);
